6055a0f6fd7f37e9e9bad0bc7e5f50c0d7152824
5910110423 <5910110423@psu.ac.th>
2019-03-24 00:29:35 +0700

    starwar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import _ from 'lodash';
+import axois from 'axios';
+import 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Card } from 'react-bootstrap';
+
+const URL = "https://swapi.co/api/species";
 
 class App extends Component {
+  constructor(prop){
+    super(prop)
+    this.state = {data : {}}
+  }
+
+  componentDidMount(){
+    axois.get(URL)
+    .then(response => {
+      this.setState( {data : response.data.results})
+      console.log(response.data)
+    })
+  }
+
+  renderSpecies(){
+    return _.map(this.state.data , spechies => {
+      return(
+        <div class = "container" >
+          <Card  bg="dark" text="white">
+          <Card.Header ><strong>{spechies.name}</strong></Card.Header>
+          <Card.Body>
+            <Card.Text><strong>classification : </strong>{spechies.classification}</Card.Text>
+            <Card.Text><strong>designation : </strong>{spechies.designation}</Card.Text>
+            <Card.Text><strong>average_height : </strong>{spechies.average_height}</Card.Text>
+            <Card.Text><strong>eye_colors : </strong>{spechies.eye_colors}</Card.Text>
+            <Card.Text><strong>skin_colors : </strong>{spechies.skin_colors}</Card.Text>
+            <Card.Text><strong>language : </strong>{spechies.language }</Card.Text>
+          </Card.Body>
+        </Card>
+        <br></br>
+        </div>
+      )
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <img src="http://pngimg.com/uploads/star_wars_logo/star_wars_logo_PNG32.png"/>;
+        {this.renderSpecies()}
       </div>
     );
   }
